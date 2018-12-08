e74c9c2f0b364abea9d26b58ac224cd004fb8206
Amine Mersoul <amersoul@e1r6p1.1337.ma>
2018-12-08 16:11:32 +0100

    adding zoom follow the mouse and reset

--- fractals/ft_deal_key.c
+++ fractals/ft_deal_key.c
@@ -6,7 +6,7 @@
 /*   By: amersoul <amersoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 12:30:07 by amersoul          #+#    #+#             */
-/*   Updated: 2018/12/07 15:44:40 by amersoul         ###   ########.fr       */
+/*   Updated: 2018/12/08 15:39:20 by amersoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,12 +70,21 @@ int		ft_deal_key(int key, void *param)
 	{
 		params->color_shift += 0.3;
 	}
-
 	if (key == 121)
 	{
 		params->color_shift -= 0.3;
 	}
-
+	if (key == 15)
+	{
+		params->view_port.h.p1 = -2;
+		params->view_port.h.p2 = 2;
+		params->view_port.v.p1 = -2;
+		params->view_port.v.p2 = 2;
+		params->scale = 1;
+		params->move = 1;
+		params->m_x = 400;
+		params->m_y = 600;
+	}
 	ft_redraw(param);
 	// ft_putnbr(key);
 	// ft_putstr(", ");
--- fractals/ft_deal_mouse.c
+++ fractals/ft_deal_mouse.c
@@ -6,7 +6,7 @@
 /*   By: amersoul <amersoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 17:23:44 by amersoul          #+#    #+#             */
-/*   Updated: 2018/12/06 17:59:20 by amersoul         ###   ########.fr       */
+/*   Updated: 2018/12/08 15:34:52 by amersoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,34 +29,26 @@ static void	mouse_up(t_draw_params *params)
 	params->move *= 0.9;
 }
 
-static void	mouse_shift_down(t_draw_params *params)
-{
-	params->scale *= 0.5;
-	params->move *= 1.5;
-}
-
-static void	mouse_shift_up(t_draw_params *params)
-{
-	params->scale *= 1.5;
-	params->move *= 0.5;
-}
-
 int			ft_deal_mouse(int button, int x, int y, void *param)
 {
 	t_draw_params *params;
-
-    x = 0;
-    y = 0;
-
 	params = (t_draw_params*)param;
 	if (button == 5)
+	{
+		params->view_port.h.p1 -= ft_map(x, 0, WIN_WIDTH, 0, 0.45) * params->move;
+		params->view_port.h.p2 -= ft_map(x, 0, WIN_WIDTH, 0, 0.45) * params->move;
+		params->view_port.v.p1 -= ft_map(y, 0, WIN_HEIGHT, 0, 0.45) * params->move;
+		params->view_port.v.p2 -= ft_map(y, 0, WIN_HEIGHT, 0, 0.45) * params->move;
 		mouse_down(params);
+	}
 	if (button == 4)
+	{
+		params->view_port.h.p1 += ft_map(x, 0, WIN_WIDTH, 0, 0.45) * params->move;
+		params->view_port.h.p2 += ft_map(x, 0, WIN_WIDTH, 0, 0.45) * params->move;
+		params->view_port.v.p1 += ft_map(y, 0, WIN_HEIGHT, 0, 0.45) * params->move;
+		params->view_port.v.p2 += ft_map(y, 0, WIN_HEIGHT, 0, 0.45) * params->move;
 		mouse_up(params);
-	if (button == 7)
-		mouse_shift_down(params);
-	if (button == 6)
-		mouse_shift_up(params);
+	}
 	ft_redraw(params);
 	return (0);
 }
